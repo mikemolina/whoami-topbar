d84a2bd91882457a251eca15dddd6de26e858c6f
Mike Molina <mmolina.unphysics@gmail.com>
2026-01-11 00:24:30 -0500

    Copyright extended to 2026

--- src/WhoamiButton.js
+++ src/WhoamiButton.js
@@ -2,7 +2,7 @@
  * WhoamiButton.js
  *
  * Whoami top bar GNOME Extension
- * Copyright (C) 2024-2025 Miguel Molina
+ * Copyright (C) 2024-2026 Miguel Molina
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
